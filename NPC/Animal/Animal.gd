f1de0e51d4b0013bff4fc1c33546b45175f6c099
KenzieLippa <klippa@bu.edu>
2023-04-11 13:04:58 -0400

    Added a green cow animal
    next will be trying to get the cow to move around maybe, not sure how ill do tht
    also to get it to play its animations

---
+++ NPC/Animal/Animal.gd
@@ -0,0 +1,9 @@
+extends Interactable
+class_name Animal
+
+#might need these but am unsure
+export(String) var animalName
+export(String, MULTILINE) var description
+
+func _run_interaction():
+	pass
