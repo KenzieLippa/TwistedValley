9cb9c554ced41e9c68750002358482cf1a6c3eda
KenzieLippa <klippa@bu.edu>
2023-04-24 17:37:03 -0400

    changed the grass and fixed the reapable scenary

--- ReapableScenery.gd
+++ ReapableScenery.gd
@@ -7,7 +7,7 @@ extends FarmItem
 onready var collision_shape_2d = $CollisionShape2D
 #onready var position_2d = $Sprite/Position2D
 
-onready var pause = $Pause
+const PAUSE = 0.1
 
 #dont try to start another animation
 var is_animating : bool = false
@@ -25,17 +25,27 @@ func _ready():
 #	pass
 
 
-func _on_ReapableScenery_area_entered(area):
+func _on_ReapableScenery_area_entered(body):
 	if is_animating == false:
 		#possibly put in a check to see which first
-		rotate_left()
+		if body.global_position.x < self.global_position.x:
+			rotate_left()
+		else:
+			rotate_right()
 		
 		
 func rotate_left():
 	var left_rotate = rot_left
-	var right_rotate = rot_right + abs(rot_left)
+	var start = sprite.rotation_degrees
 	var tween = create_tween() #can set special properties later
-	tween.tween_property(sprite, "rotation_degrees", left_rotate, 0.1)
+	tween.tween_property(sprite, "rotation_degrees", left_rotate, PAUSE)
 	#yield(tween, "finished")
-	tween.tween_property(sprite, "rotation_degrees", right_rotate, 0.1)
+	tween.tween_property(sprite, "rotation_degrees", start, PAUSE)
 	#yield(tween, "finished")
+
+func rotate_right():
+	var right_rotate = rot_right
+	var start = sprite.rotation_degrees
+	var tween = create_tween()
+	tween.tween_property(sprite, "rotation_degrees", right_rotate, PAUSE)
+	tween.tween_property(sprite, "rotation_degrees", start, PAUSE)
